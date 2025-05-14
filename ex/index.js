100e723e0e1fab9f67c08fdd157029b0d16aa71e
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-05-14 14:09:15 -0300

    feat: babel e imports

--- ex/index.js
+++ ex/index.js
@@ -1,2 +1,7 @@
+import Pessoa from "./pessoa"
+
 const logger = require("./funcaoRuim")
 logger.nada("Usando Padrão CommonJS!")
+
+const pessoa = new Pessoa("Guilherme")
+console.log(Pessoa.toString())
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,30 @@
-const webpack = require("webpack")
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
     entry: "./ex/index.js",
     output: {
-        path: __dirname + "/public",
-        filename: "./bundle.js"
+        path: path.resolve(__dirname, "public"),
+        filename: "bundle.js"
     },
     devServer: {
         port: 8080,
-        contentBase: "./public"
+        static: {
+            directory: path.join(__dirname, "public")
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // ou /\.jsx?$/ se quiser incluir JSX
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+                query: {
+                    presets: ["es2015"]
+                }
+            }
+        ]
     }
 }
